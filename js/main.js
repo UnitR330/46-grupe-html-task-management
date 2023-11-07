228a56008339d29fdacbe87c70e28523335a6b74
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-11-07 23:13:35 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
-// LIETUVA - automobilis, sunkvezimis
-import { minus, multi, sum } from './sum.js';
+import { Todo } from "./todo.js";
 
-console.log('labas');
+const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In pogress', 'Done']);
 
-const n1 = 7;
-const n2 = 5;
+// console.log(kanban);
 
-
-const n12 = sum(n1, n2);
-const n12minus = minus(n1, n2);
-const n12multi = multi(n1, n2);
-
-console.log(n12);
-console.log(n12minus);
-console.log(n12multi);
+// kanban.addTask('');
