9b21eaf7cef6950aa7c8ff1931d506004c84e845
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-11-09 22:06:47 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,72 @@
+import { Todo } from "./todo.js";
+
+const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']);
+
+const addTaskBtnDOM = document.getElementById('add_task');
+const asideDOM = document.getElementById('aside');
+const asideBackgroundDOM = asideDOM.querySelector('.aside-bg');
+const asideCloseBtnDOM = asideDOM.querySelector('.aside-header button');
+asideDOM.classList.add('show');
+
+if (addTaskBtnDOM && asideDOM) {
+    addTaskBtnDOM.addEventListener('click', () => {
+        asideDOM.classList.add('show');
+    })
+
+    asideBackgroundDOM.addEventListener('click', () => {
+        asideDOM.classList.remove('show');
+    })
+
+    asideCloseBtnDOM.addEventListener('click', () => {
+        asideDOM.classList.remove('show');
+    })
+
+    window.addEventListener('keyup', (event) => {
+        if (event.key === 'Escape') {
+            asideDOM.classList.remove('show');
+        }
+        if (event.key === '+') {
+            asideDOM.classList.add('show');
+        }
+    })
+}
+
+const formDOM = document.getElementById('task_form');
+const formTitleDOM = document.getElementById('title');
+const formDescDOM = document.getElementById('desc');
+const formDeadlineDOM = document.getElementById('deadline');
+const formTagsDOM = document.getElementById('tags');
+if (formDOM) {
+    formDOM.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        kanban.addTask({
+            columnIndex: 0,
+            title: formTitleDOM.value,
+            desc: formDescDOM.value,
+            createdOn: '2023-11-08 09:03:15',
+            deadline: formDeadlineDOM.value,
+            tags: formTagsDOM.value
+                .split(',')
+                .filter(txt => txt !== '')
+                .map(txt => ({ text: txt.trim(), color: '#333' }))
+        });
+
+        formTitleDOM.value = '';
+        formDescDOM.value = '';
+        formDeadlineDOM.value = '';
+        formTagsDOM.value = '';
+    })
+}
+
+
+
+console.log(kanban);
+
+
+
+
+/*
 import { Todo } from "./Todo.js";
 
 const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']);
@@ -44,3 +113,5 @@ console.log(kanban);
 // kanban.addTask('');
 // pridedam 1 + 
 //importojam N jau turimu
+
+*/
--- js/todo.js
+++ js/todo.js
@@ -17,6 +17,7 @@ export class Todo {
         }
 
         this.render();
+        this.loadInitialData();
     }
 
     updateDOMelement() {
@@ -44,6 +45,112 @@ export class Todo {
 
         this.columnsDOM = this.DOM.querySelectorAll('.task-list');
     }
+
+    taskCardHTML(taskID, task) {
+        let tagsHTML = '';
+
+        for (const tag of task.tags) {
+            tagsHTML += `<div class="tag" style="color: ${tag.color};">${tag.text}</div>`;
+        }
+
+        return `
+            <li id="task_${taskID}" class="task-card">
+                <div class="task-actions">
+                    <button class="fa fa-trash"></button>
+                </div>
+                <div class="task-title">${task.title}</div>
+                <div class="task-desc">${task.desc}</div>
+                <div class="task-tags">${tagsHTML}</div>
+                <div class="task-deadline">${task.deadline}</div>
+            </li>`;
+    }
+
+    addTask(task) {
+        this.renderTask(task);
+        localStorage.setItem('46g-task-list', JSON.stringify(this.tasks));
+    }
+
+    renderTask(task) {
+        const taskID = ++this.lastUsedtaskId;
+        this.tasks.push({
+            ...task,
+            isDeleted: false,
+        });
+
+        this.columnsDOM[task.columnIndex].insertAdjacentHTML('beforeend', this.taskCardHTML(taskID, task));
+
+        const taskDOM = document.getElementById(`task_${taskID}`);
+        const deleteButtonDOM = taskDOM.querySelector('.fa-trash');
+
+        deleteButtonDOM.addEventListener('click', () => {
+            this.tasks[taskID - 1].isDeleted = true;
+            taskDOM.remove();
+        });
+    }
+
+    loadInitialData() {
+        const localData = localStorage.getItem('46g-task-list');
+        const data = JSON.parse(localData);
+
+        for (const task of data) {
+            this.renderTask(task);
+        }
+    }
+}
+
+
+
+
+
+/*
+export class Todo {
+    constructor(selector, columns) {
+        this.selector = selector;
+        this.columns = columns;
+        this.DOM = null;
+        this.columnsDOM = [];
+        this.tasks = [];
+        this.lastUsedtaskId = 0;
+
+        this.init();
+    }
+
+    init() {
+        const err = this.updateDOMelement();
+        if (typeof err === 'string') {
+            return console.error(err);
+        }
+
+        this.render();
+    }
+
+    updateDOMelement() {
+        if (typeof this.selector !== 'string' || this.selector === '') {
+            return 'Netinkamas selektorius';
+        }
+
+        this.DOM = document.querySelector(this.selector);
+    }
+
+    render() {
+        let HTML = '';
+
+        for (const column of this.columns) {
+            HTML += `
+                <div class="column">
+                    <h2 class="title">${column}</h2>
+                    <ul class="task-list"></ul>
+                </div>`;
+        }
+
+        this.DOM.classList.add('todo');
+        this.DOM.innerHTML = HTML;
+        this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
+
+        this.columnsDOM = this.DOM.querySelectorAll('.task-list');
+    }
+taskCardHTML = '';
+
 
     addTask(task) {
         this.tasks.push({
@@ -80,7 +187,7 @@ export class Todo {
         });
     }
 }
-
+*/
 
 
 /*   this.DOM.style.backgroundColor = 'yellow';
