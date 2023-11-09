423330e42f7eb0f8e618219aa6745408df802ccf
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-11-09 08:28:55 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
-import { Todo } from "./todo.js";
+import { Todo } from "./Todo.js";
 
-const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In pogress', 'Done']);
+const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']);
+
+kanban.addTask({
+    columnIndex: 1,
+    title: 'Pirmos uzduoties atraste',
+    desc: 'Pirmos uzduoties pilnas aprasas... laaaabai issamiai nupasakoja ka reikia padaryti ;)',
+    createdOn: '2023-11-08 09:03:15',
+    deadline: '2023-12-24 00:00:00',
+    tags: [
+        { text: 'Design', color: '#333' },
+    ],
+});
+
+kanban.addTask({
+    columnIndex: 0,
+    title: 'Antra uzduotis',
+    desc: 'Antros uzduoties pilnas aprasas... laaaabai issamiai nupasakoja ka reikia padaryti ;)',
+    createdOn: '2023-11-08 10:03:15',
+    deadline: '2023-12-24 00:00:00',
+    tags: [
+        { text: 'UX', color: '#f00' },
+        { text: 'UI', color: '#090' },
+    ],
+});
+
+kanban.addTask({
+    columnIndex: 0,
+    title: 'Trecia uzduotis',
+    desc: 'Trecios uzduoties pilnas aprasas :P',
+    createdOn: '2023-11-08 10:13:15',
+    deadline: '2023-12-20 00:00:00',
+    tags: [
+        { text: 'Development', color: '#00c' },
+    ],
+});
+
+console.log(kanban);
+
 
 // console.log(kanban);
 
 // kanban.addTask('');
+// pridedam 1 + 
+//importojam N jau turimu
--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,9 @@ export class Todo {
         this.selector = selector;
         this.columns = columns;
         this.DOM = null;
+        this.columnsDOM = [];
+        this.tasks = [];
+        this.lastUsedtaskId = 0;
 
         this.init();
     }
@@ -29,25 +32,54 @@ export class Todo {
 
         for (const column of this.columns) {
             HTML += `
-                <div>
-                    <h2>${column}</h2>
-                    <ul>
-                        <li>Task</li>
-                        <li>Task</li>
-                        <li>Task</li>
-                        <li>Task</li>
-                        <li>Task</li>
-                    </ul>
+                <div class="column">
+                    <h2 class="title">${column}</h2>
+                    <ul class="task-list"></ul>
                 </div>`;
         }
 
         this.DOM.classList.add('todo');
         this.DOM.innerHTML = HTML;
         this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
+
+        this.columnsDOM = this.DOM.querySelectorAll('.task-list');
+    }
+
+    addTask(task) {
+        this.tasks.push({
+            ...task,
+            isDeleted: false,
+        });
+        const taskID = ++this.lastUsedtaskId;
+        let tagsHTML = '';
+
+        for (const tag of task.tags) {
+            tagsHTML += `<div class="tag" style="color: ${tag.color};">${tag.text}</div>`;
+        }
+
+        const HTML = `
+            <li id="task_${taskID}" class="task-card">
+                <div class="task-actions">
+                    <button class="fa fa-trash"></button>
+                </div>
+                <div class="task-title">${task.title}</div>
+                <div class="task-desc">${task.desc}</div>
+                <div class="task-tags">${tagsHTML}</div>
+                <div class="task-deadline">${task.deadline}</div>
+            </li>`;
+
+        // this.columnsDOM[task.columnIndex].innerHTML += HTML;
+        this.columnsDOM[task.columnIndex].insertAdjacentHTML('beforeend', HTML);
+
+        const taskDOM = document.getElementById(`task_${taskID}`);
+        const deleteButtonDOM = taskDOM.querySelector('.fa-trash');
+
+        deleteButtonDOM.addEventListener('click', () => {
+            this.tasks[taskID - 1].isDeleted = true;
+            taskDOM.remove();
+        });
     }
 }
-
-
 
 
 
